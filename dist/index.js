6841ec166dd31b73378c68e3e828bbfc5196f481
Jason <jason_nyberg@hotmail.com>
2021-07-01 08:08:09 -0500

    new build in dist folder

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Tilt", {
+Object.defineProperty(exports, "DiagonalContainer", {
   enumerable: true,
   get: function get() {
-    return _tilt.default;
+    return _diagonalContainer.default;
   }
 });
 
-var _tilt = _interopRequireDefault(require("./components/tilt"));
+var _diagonalContainer = _interopRequireDefault(require("./components/diagonalContainer"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
